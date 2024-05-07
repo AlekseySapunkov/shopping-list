0df77b229aefb574c707492b998e736a970facbf
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-05-07 19:54:05 +0300

    add Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
+import ItemCost from "./components/ItemCost";
 function App() {
   return (
     <div >
       <h1>"Начнем изучение react"</h1>
+      <ItemCost></ItemCost>
     </div>
   );
 }
---
+++ src/components/ItemCost.js
@@ -0,0 +1,12 @@
+function ItemCost() {
+  return (
+    <div>
+      <div>Март 2012</div>
+      <div>
+        <h2>Холодильник</h2>
+        <div>120$</div>
+      </div>
+    </div>
+  );
+}
+export default ItemCost;

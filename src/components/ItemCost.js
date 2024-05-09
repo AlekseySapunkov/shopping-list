6603438b880ec82fbbe2cdc8d2387b50713d5122
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-05-09 13:09:27 +0300

    update ItemCost.js

--- src/components/ItemCost.js
+++ src/components/ItemCost.js
@@ -1,12 +1,13 @@
 import "./ItemCost.css";
 function ItemCost() {
-  const costDate = new Date(2024, );
+  const costDate = new Date(2024, 4, 9);
+  const costTitle = 120;
   return (
     <div className="cost-item">
-      <div>Март 2012</div>
+      <div>{costDate.toDateString()}</div>
       <div className="cost-item__description">
         <h2>Холодильник</h2>
-        <div className="cost-item__price">120$</div>
+        <div className="cost-item__price">{costTitle }</div>
       </div>
     </div>
   );

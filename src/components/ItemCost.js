910754dbcfaa4e0dcd7e9470c39efba0bf4e2a61
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-05-11 14:50:29 +0300

    update ItemCost.css and ItemCost.js

--- src/components/ItemCost.js
+++ src/components/ItemCost.js
@@ -1,11 +1,17 @@
 import "./ItemCost.css";
 function ItemCost(props) {
-  const costDate = props.date;
+  const month = props.date.toLocaleString("ru-Ru", { month: "long" });
+  const day = props.date.toLocaleString("ru-Ru", { day: "numeric" });
+  const year = props.date.toLocaleString("ru-Ru", { year: "2-digit" });
   const costTitle = props.price;
   const title = props.description;
   return (
     <div className="cost-item">
-      <div>{costDate.toDateString()}</div>
+      <div className="cost-date">
+        <div cost-date__day>{day}</div>
+        <div className="cost-date__month">{month}</div>
+        <div className="cost-date__year">{year}</div>
+      </div>
       <div className="cost-item__description">
         <h2>{title}</h2>
         <div className="cost-item__price">{costTitle}</div>

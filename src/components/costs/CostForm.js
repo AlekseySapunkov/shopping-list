d89bf79012ef1d886b91e88f2469ab4d72f62262
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-05-16 12:57:51 +0300

    Summary

---
+++ src/components/costs/CostForm.js
@@ -0,0 +1,19 @@
+import "./CostForm.css"
+const CostForm = () => {
+    return <div>
+        <form>
+            <div className="new-cost__controls">
+            <div className="new-cost__control label">
+                <label>Название</label>
+                <input type="text" className="new-cost__control input"></input>
+            </div>
+            <div className="new-cost__control label">
+                <label>Сумма</label>
+                <input type = "" className="new-cost__control input"></input>
+            </div>
+            </div>
+            
+        </form>
+    </div>
+    }
+    export default CostForm
---
+++ src/components/costs/NewCost.js
@@ -0,0 +1,8 @@
+import "./NewCost.css"
+import CostForm  from "./CostForm"
+const NewCost = () => {
+return <div>
+    <CostForm></CostForm>
+</div>
+}
+export default NewCost

33f4ad31b10a3048a2910d836be920e1011fb029
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-06-02 15:48:18 +0300

    add UseState hook

--- src/components/costs/CostForm.js
+++ src/components/costs/CostForm.js
@@ -13,19 +13,31 @@ const CostForm = () => {
     const dateChangeHandler =(event)=>{
         setDate(event.target.value)
     };
-    return <form>
+    const submitHandler =(event)=>{
+        event.preventDefault();
+        const costData = {
+            name:name,
+            date:date,
+            amount:amount,
+        }
+        console.log(costData);
+        setName('');
+        setDate('');
+        setAmount('');
+    }
+    return <form onSubmit={submitHandler}>
          <div className="new-cost__controls">
          <div className="new-cost__control">
                 <label>Название</label>
-                <input type="text" onChange={nameChangeHandler}></input>
+                <input type="text" onChange={nameChangeHandler} value={name}></input>
             </div>
             <div className="new-cost__control">
             <label>Сумма</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={amount}/>
             </div>
             <div className="new-cost__control">
             <label>Дата</label>
-                <input type="date" min="0.01" step="0.01" onChange={dateChangeHandler}/>
+                <input type="date" min="0.01" step="0.01" onChange={dateChangeHandler} value={date}/>
             </div>
             <div className="new-cost__actions"> 
 <button type ="submit">Добавить расход</button>

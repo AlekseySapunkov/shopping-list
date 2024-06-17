3cab969bf27761a1e4787d471b94d432cd232a36
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-06-17 13:28:01 +0300

    add year filter

--- src/components/costs/Costs.js
+++ src/components/costs/Costs.js
@@ -4,16 +4,20 @@ import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
 const Costs = (props) => {
-  const [selectedYear, setSelectedYear] = useState("2020");
+  const [selectedYear, setSelectedYear] = useState("2024");
   const yearChangeHandler = (year) => {
     setSelectedYear(year);
   };
-  console.log(props)
+  console.log(props.costs)
+  console.log(props.costs.map((cost)=>cost.date.getUTCFullYear()))
+  const newArr= props.costs.filter((cost)=>cost.date.getUTCFullYear()==selectedYear)
+  console.log(newArr)
+  console.log(selectedYear)
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        {props.costs.map((cost)=> 
+        {props.costs.filter((cost)=>cost.date.getUTCFullYear()==selectedYear).map((cost)=> 
         <ItemCost
          key={cost.id}
         date={cost.date}

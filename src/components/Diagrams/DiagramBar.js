9f7d1140e53615dd09bcbe6b457eab2c50b5d1b4
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-06-21 16:40:52 +0300

    Update DiagramBar.js

--- src/components/Diagrams/DiagramBar.js
+++ src/components/Diagrams/DiagramBar.js
@@ -2,11 +2,11 @@ import "./DiagramBar.css"
 const DiagramBar = (props)=> {
     let barFillLine = "0%";
     if(props.maxValue > 0){
-        
+        barFillLine = Math.round((props.value / props.maxValue) *100) + '%'
     }
 return (<div className="diagram-bar">
 <div className="diagram-bar__inner">
-    <div className="diagram-bar__fill"></div>
+    <div className="diagram-bar__fill" style={{height : barFillLine}}></div>
 </div>
 <div className="diagram-bar__label"></div>
     </div>)

74a50c1124763d6b98591a936009bd4c8cb8de71
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-06-21 12:33:34 +0300

    add  styles in Components DiagramBar and Diagram

---
+++ src/components/Diagrams/Diagram.js
@@ -0,0 +1,8 @@
+import DiagramBar  from "./DiagramBar";
+import "./Diagram.css"
+const Diagram = (props)=> {
+return (<div className="diagram">
+    <DiagramBar/>
+    </div>)
+}
+export default Diagram;

9351b077e31b277d741b16e70c80c17c98aa63a4
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-12-16 12:49:37 +0300

    add model and controller

--- golang_server/main.go
+++ golang_server/main.go
@@ -9,6 +9,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 func main() {
+	http.HandleFunc("/items", getItems)
+	http.HandleFunc("/items/create", createItem)
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)

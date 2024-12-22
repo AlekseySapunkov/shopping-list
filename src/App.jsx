529037df89fba149afca64bd7fb56114c6c98107
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-12-22 16:47:54 +0300

    fix App.jsx delete hard code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
 import React,{useEffect, useState} from "react";
 import Costs from "./components/costs/Costs";
 import NewCost from "./components/costs/NewCost";
-const initialCosts = [
-  {
-    id:'c1',
-    date: new Date(2024, 4, 9),
-    description: "freezer",
-    price: 120,
-  },
-  {
-    id:'c2',
-    date: new Date(2024, 5, 9),
-    description: "camera",
-    price: 220,
-  },
-  {
-    id:'c3',
-    date: new Date(2024, 4, 9),
-    description: "Macbook",
-    price: 920,
-  },
-];
+
 const App = () => {
+  let savedItems = []
   const getApi = async()=>{
      const connectionResponse = await fetch("http://localhost:5200/items")
      return connectionResponse.json()
   }
 useEffect(()=>{
   getApi()
-  .then((res) => console.log(res))
+  .then((res) => {
+    res.forEach(element => {
+      const {id, item_name:description, date, price} = element
+      savedItems.push({id, description, date:new Date(date), price})
+    });
+  })
   .catch((err) => console.log(err));
 },[])
-  const[costs,setCosts]=useState(initialCosts);
+console.log(init)
+const[costs,setCosts]=useState(savedItems);
   const onAddCostHandler =(cost)=>{
     console.log(cost)
     setCosts(prevCosts=>{

10f9be4f3376db13ddf86aae3b8f071d9e7453b8
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-12-23 16:37:04 +0300

    Create DeleteApi.js

---
+++ src/services/DeleteApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const deleteApiById = async (id) => {
+  try {
+    const response = await axios.delete(`http://localhost:5200/items/${id}`);
+    return response.data.id
+  } catch (error) {
+    console.error(error);
+  }
+}
+export default deleteApiById;

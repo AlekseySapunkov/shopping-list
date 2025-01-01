6f11e24236062c7780dda94cec0cea126d75e3dc
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-01 13:50:02 +0300

    rewrite app using vite instead of webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Costs from "./components/costs/Costs";
 import NewCost from "./components/costs/NewCost";
 import getApi from "./services/GetApi";
 const App = () => {
-  let savedItems = []
-useEffect(()=>{
-  getApi()
-  .then((res) => {
-    res.forEach(element => {
-      const {id, item_name:description, date, price} = element
-      savedItems.push({id, description, date:new Date(date), price})
+  let savedItems = [];
+  useEffect(() => {
+    getApi()
+      .then((res) => {
+        res.forEach((element) => {
+          const { id, item_name: description, date, price } = element;
+          savedItems.push({ id, description, date: new Date(date), price });
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const [costs, setCosts] = useState(savedItems);
+  const onAddCostHandler = (cost) => {
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
     });
-  })
-  .catch((err) => console.log(err));
-},[])
-const[costs,setCosts]=useState(savedItems);
-  const onAddCostHandler =(cost)=>{
-    setCosts(prevCosts=>{
-      return [cost,...prevCosts]
-    });
-  }
+  };
   return (
     <div>
-      <NewCost onAddCost= {onAddCostHandler} value={true}/>
+      <NewCost onAddCost={onAddCostHandler} value={true} />
       <Costs costs={costs} />
     </div>
   );

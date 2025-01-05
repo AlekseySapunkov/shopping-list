092e40379ea12e43291f5e683eea14af69231ec2
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-05 14:05:15 +0300

    Summary

--- node_server/app.js
+++ node_server/app.js
@@ -8,6 +8,7 @@ app.use(cors());
 pool.query("SELECT NOW()", (err, result) => {
   if (err) {
     console.error("error connecting to the database", err.stack);
+    console.log(process.env.DB_USER);
   } else {
     console.log("Success connection to the database", result.rows);
   }

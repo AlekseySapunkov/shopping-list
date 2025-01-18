e40a59de3ae071b49e95f0c4611b6761cd39461a
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-18 14:45:12 +0300

    add test-trensform-css

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 export default {
-    testEnvironment : 'jest-environment-jsdom',
-    extensionsToTreatAsEsm: ['.jsx'],
-}
+  testEnvironment: "jest-environment-jsdom",
+  extensionsToTreatAsEsm: [".jsx"],
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+    "^.+\\.css$": "jest-transform-css",
+  },
+};

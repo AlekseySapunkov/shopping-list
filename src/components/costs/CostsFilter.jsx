b9ca5eab1643e9af5acba250b496eb288c6da796
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-23 09:05:28 +0300

    refactory:добавить label атрибут

--- src/components/costs/CostsFilter.jsx
+++ src/components/costs/CostsFilter.jsx
@@ -7,8 +7,8 @@ const CostsFilter = (props) => {
   return (
     <div className="costs-filter">
       <div className="costs-filter__control">
-        <label>Выбор По Году</label>
-        <select value={props.year} onChange={yearChangeHandler}>
+        <label htmlFor="dropdown-choice-year">Выбор По Году</label>
+        <select id="dropdown-choice-year" value={props.year} onChange={yearChangeHandler}>
           <option value="2025">2025</option>
           <option value="2024">2024</option>
           <option value="2021">2021</option>

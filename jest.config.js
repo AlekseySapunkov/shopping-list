2ba8934f80c4af013e67a1ec3842866fcea67974
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-28 12:48:42 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 export default {
   testEnvironment: "jest-environment-jsdom",
   extensionsToTreatAsEsm: [".jsx"],
+  globals: {
+    axios: require("axios"),
+   },
   transform: {
     "\\.[jt]sx?$": "babel-jest",
     ".+\\.(css|scss)$": "jest-css-modules-transform"

a16d5d6dd8388576edf3247589a482d118a9ef5a
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-02-05 12:55:57 +0300

    Update DeleteApi.test.js

--- src/__tests__/DeleteApi.test.js
+++ src/__tests__/DeleteApi.test.js
@@ -1,6 +1,7 @@
 import deleteApiById from "../services/DeleteApi";
+import axios from "axios";
 import jest from "jest"
-jest.mock("axios")
+jest.mock('axios')
 describe('удаление элемента из базы данных',()=>{
     test("test delete api", async ()=>{
        const response = await deleteApiById(3)

a1758dde4582ecc6216edc04d3be3be96b97f785
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-02-12 16:18:09 +0300

    Update ItemCost.test.jsx

--- src/__tests__/ItemCost.test.jsx
+++ src/__tests__/ItemCost.test.jsx
@@ -2,6 +2,14 @@ import ItemCost from "../components/costs/ItemCost";
 import { render } from "@testing-library/react";
 describe("ItemCost component", () => {
   test("ItemCost should be rendered", () => {
-    render(<ItemCost />);
+    const props={
+      key: 10,
+      id: 10,
+      date: new Date('2022-05-25 9:00'),
+      description: 'бла бла',
+      price: 35,
+    }
+
+    render(<ItemCost {...props}/>);
   });
 });

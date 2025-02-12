c7d0f26141bac5a535fa8b60f74e7d2ca3b9db13
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-02-12 16:50:18 +0300

    Update ItemCost.test.jsx

--- src/__tests__/ItemCost.test.jsx
+++ src/__tests__/ItemCost.test.jsx
@@ -3,13 +3,12 @@ import { render } from "@testing-library/react";
 describe("ItemCost component", () => {
   test("ItemCost should be rendered", () => {
     const props={
-      key: 10,
       id: 10,
       date: new Date('2022-05-25 9:00'),
       description: 'бла бла',
       price: 35,
     }
-
-    render(<ItemCost {...props}/>);
+    const {container}= render(<ItemCost key = {10} {...props}/>);
+    expect(container.getElementsByClassName('cost-date__day')).toBe([<div class="cost-date__day">25</div>])
   });
 });
